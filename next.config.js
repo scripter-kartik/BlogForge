7d442a2bc9de5165a6c5c9b153a150565f590802
scripter-kartik <agarwalkartik704@gmail.com>
2026-01-01 18:07:30 +0530

    disable turbopack to support custom webpack config

--- next.config.js
+++ next.config.js
@@ -21,7 +21,43 @@ const nextConfig = {
         hostname: 'lh3.googleusercontent.com',
         pathname: '/**',
       },
+    ],/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: false, 
+  },
+
+  outputFileTracingRoot: require('path').join(__dirname),
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.cache = false;
+    }
+    return config;
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
     ],
+  },
+
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
+  },
+};
+
+module.exports = nextConfig;
+
   },
   
   env: {
