4bb132a562c131de8a9330fd54684dabdbca4c53
scripter-kartik <agarwalkartik704@gmail.com>
2026-01-01 18:08:48 +0530

    fix: correct next.config syntax and disable turbopack

--- next.config.js
+++ next.config.js
@@ -1,33 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: require('path').join(__dirname),
-  
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.cache = false;
-    }
-    return config;
-  },
-  
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        pathname: '/**',
-      },
-    ],/** @type {import('next').NextConfig} */
-const nextConfig = {
   experimental: {
-    turbo: false, 
+    turbo: false,
   },
 
-  outputFileTracingRoot: require('path').join(__dirname),
+  outputFileTracingRoot: require("path").join(__dirname),
 
   webpack: (config, { isServer }) => {
     if (!isServer) {
@@ -39,29 +16,20 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
       },
     ],
   },
 
   env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
-  },
-};
-
-module.exports = nextConfig;
-
-  },
-  
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "http://localhost:3000",
   },
 };
 
